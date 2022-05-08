f47484b0de131d109f08887e01257fd43f36a128
KerliS9 <kerlischroeder9@gmail.com>
2022-05-07 21:15:51 -0300

    adicionando botões de enviar e foto

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -35,12 +35,19 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
 
+    function handleRestartFeedback() {
+        setFeedbackType(null);
+    }
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             {!feedbackType ? (
                 <FeedbackTypesStep onFeedbackTypeChanged={ setFeedbackType } />
             ) : (
-                <FeedbackContentStep feedbackType={ feedbackType } />
+                <FeedbackContentStep
+                    feedbackType={ feedbackType }
+                    onFeedbackRestartRequested={ handleRestartFeedback }
+                />
             )}
             <footer className="text-xs text-neutral-400">
                 Feito com ♥ por <a className="underline underline-offset-2" href="https://www.linkedin.com/in/kerlischroeder/">Kerli Schroeder</a>
