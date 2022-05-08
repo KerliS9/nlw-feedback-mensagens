46569a9fbd419bd05357f0c146071e0a1f2a062a
KerliS9 <kerlischroeder9@gmail.com>
2022-05-07 21:42:05 -0300

    configurando botão para tirar foto

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { CloseButton } from '../../CloseButton';
 import { ScreenshotButton } from '../ScreenshotButton';
 import { feedbackTypes, FeedbackType } from '..';
@@ -9,6 +10,8 @@ interface FeedbackContentStepProps {
 }
 
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     return (
@@ -33,7 +36,7 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                     placeholder="Conte com detalhes o que está acontecendo"
                 />
                 <footer className="flex gap-2 mt-2">
-                    <ScreenshotButton />                  
+                    <ScreenshotButton onScreenshotTook={ setScreenshot } />                  
                     <button
                         type="submit"
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
